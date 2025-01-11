dc5641618d85ade64d0e0475c47770149774b6f3
hyfhaozero <hyfhao@outlook.com>
2025-01-11 09:09:09 +0800

    do a test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -12,7 +10,12 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // 设置路径别名
     },
   },
+  server: {
+    host: '0.0.0.0',  // 允许所有 IP 访问
+    port: 8080,       // 设置端口号
+  },
 })
+
