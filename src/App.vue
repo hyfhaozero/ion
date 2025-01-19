c099067cc4ae49b51b14738962493a19334cb59c
hyfhaozero <hyfhao@outlook.com>
2025-01-19 11:27:32 +0800

    clean

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import { useNoticeStore } from '@/stores/notice_pb'
-const store_notice = useNoticeStore()
-</script>
-
-<template>
-  <p>msg: {{ store_notice.notice }}</p>
-</template>
